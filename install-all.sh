d95474b196d0194ebe2ac1611984b6580f4c524e
Александр <a-x-@users.noreply.github.com>
2015-09-12 17:00:52 +0300

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -4,3 +4,6 @@
 < ~/Cloud/apps/brew.list sed -E 's/#.+//' | xargs brew install
 < ~/Cloud/apps/npm.list sed -E 's/#.+//' | xargs npm -g install
 < ~/Cloud/apps/brew-cask.list sed -E 's/#.+//' | xargs brew cask install
+
+echo "\nСписки brew, npm, brew-cask установлены."
+echo "\nЗайдите в appstore во вкладку «купленных» приложений и нажмите установить против каждой необходимой."
