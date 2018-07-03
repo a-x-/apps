057844f0ef1f700848a4f96cab2fb27bcdeec04f
alex <alex@rocketguys.com>
2018-07-03 21:22:30 +0300

    update npm.list

--- npm.list.generator.sh
+++ npm.list.generator.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-npm -g list -depth 0 | sed '1d' | cut -d ' ' -f 2 | cut -d '@' -f 1 | grep -vE 'npm|install' > npm.list
+npm -g list -depth 0 2>/dev/null   | sed '1d' | cut -d ' ' -f 2 | cut -d '@' -f 1 | grep -vE 'npm|install' > npm.list
